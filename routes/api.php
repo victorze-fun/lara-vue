5c3078011afb80ff497f529f8a0e9f4ffe847cd6
Victor <victorze@protonmail.com>
2021-10-25 17:47:32 -0500

    Add routes

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,7 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::resource('blogs', App\Http\Controllers\BlogController::class)->except([
+    'create', 'edit'
+]);
